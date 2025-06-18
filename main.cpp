94187577b345a39a0411b9ae08be6a2cbb80cbc4
YehorPylypenko <stadeavtodan@gmail.com>
2025-06-18 18:07:33 +0200

    Task 2 - created computer structure Task 3 - Created 5 examples

--- main.cpp
+++ main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
+#include "Computer.h"
+using namespace std;
 
 int main()
 {
-
+    int n = 5;
+    Computer* arr = new Computer[n]{
+            {"Asus", 8, 9999.99},
+            {"HP", 16, 19999.99},
+            {"Lenovo", 4, 29999.99},
+            {"MacBook", 32, 49999.99},
+            {"Acer", 8, 39999.99}
+    };
 }
